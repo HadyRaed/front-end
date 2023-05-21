6f4dbb87815f6bbf0c3f9c8188c0913e22bce47f
hadyraed <hraed666@gmail.com>
2023-05-21 19:59:44 +0200

    intial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,21 @@
+import './globals.css'
+import { Nunito } from 'next/font/google'
+
+const inter = Nunito({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Venues',
+  description: 'Venues real estate ',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+        <h1 className="text-rose-500"> hello venues</h1>
+  )
+}
